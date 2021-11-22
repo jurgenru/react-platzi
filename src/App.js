8f5572c5a5cd036256562de5d8df3ee4a913013a
jurgenru <jurgenruegnberg@gmail.com>
2021-11-22 14:38:36 -0400

    Funcion Search

--- src/App.js
+++ src/App.js
@@ -14,19 +14,39 @@ contenido*/
 //gracias a nuestros todos un TodoItem
 
 //creamos un array con objetos dentro
-const todos = [
+const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
   { text: "Termina la universidad", completed: false },
   { text: "Cortarme el pelo", completed: false },
+  { text: "Estudia react", completed: false },
+  { text: "Aprender react", completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodo = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodo = todos;
+  } else {
+    searchedTodo = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
   return (
     <>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter total={totalTodos} completed={completedTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodo.map((todo) => (
           <TodoItem
             text={todo.text}
             key={todo.text}
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,24 +1,20 @@
 import React from "react";
 import "./TodoSearch.css";
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState("");
-
+function TodoSearch({searchValue, setSearchValue}) {
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
     setSearchValue(event.target.value);
   };
 
-  return [
+  return (
     <input
-      onChange={onSearchValueChange}
       className="TodoSearch"
       placeholder="Busca tu TODO"
       value={searchValue}
       onChange={onSearchValueChange}
-    />,
-    <p>{searchValue}</p>,
-  ];
+    />
+  );
 }
 
 export { TodoSearch };
